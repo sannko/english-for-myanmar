b13db363e41caa4bdbe2b16c066bdf4273ff9c5d
sannko <sannkokooo@gmail.com>
2022-07-01 05:47:12 +0630

    Initial commit

---
+++ src/app/grammar/data.ts
@@ -0,0 +1,34 @@
+export class Data {
+
+    courseContent = [
+        "am / is / are", // 1
+        "am / is / are (questions)", // 2
+        "I am doing (present continous)", //3 
+        "are you doing? (present continous question)", //4 
+        "I do / work / like etc. (present simple)", //5
+        "I don't ... (present simple negative)", //4
+        "Do you ... ? (present simple questions)", //7
+        "I am doing (present continous) and I do (present simple)", //8
+        "I have ... and I've got ...", //9
+        "was / were", //10
+        "worked / got / went etc. (past simple)", //11
+        "I didn't ... Did you ...?(past simple negative and questions)", //12
+        "I was doing (past continous)", //13
+        "I was doing (past continous) and I did (past simple)", //14
+        "I have done (present perfect 1)", //15
+        "I've just ... I've already ... I haven't ... yet (present perfect 2)", //16
+        "Haver you ever ... ? (present perfect 3)", //17
+        "How long have you ... ? (present perfect 4)", //18
+        "for since ago", //19
+        "I have done(present perfect) and I did (past)", //20
+        "is done was done (passive 1)", //21
+        "is being done has been done (passive 2)", //22
+        "be / have / do in present and past tenses", //23
+        "Regular and irregular verbs", //24
+        "What are you doing tomorrow?", //25
+        
+
+         
+    ]
+
+}
