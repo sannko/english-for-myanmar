4c74836b987bf1d623217458724544febc319bca
sannko <sannkokooo@gmail.com>
2022-07-01 06:51:46 +0630

    Initial commit

--- src/app/grammar/data.ts
+++ src/app/grammar/data.ts
@@ -51,71 +51,71 @@ export class Data {
         {id:48, link:"", name:"How long does it take ... ?"}, // 48
         {id:49, link:"", name:"Do you know where ... ? I don't know what ... etc"}, //49
         {id:50, link:"", name:"She said that ... He told me that ..."}, //50
-        "work / working go / going do / doing", //51
-        "to ... (I want to do) and -ing (I enjoy doing)", //52
-        "I want to ... I told you to ...", //53
-        "I went to the shop to ...", //54
-        "go to ... go on .. go for ... go -ing", //55
-        "get", //56
-        "do and make", //57
-        "have", //58
-        "I / me he / him they / them etc.", //59
-        "my / his / their etc.", //60
-        "Whose is this? It's mine / yours / hers etc.", //61
-        "I / me / my / mine", //62
-        "myself / yourself / themselves etc.", //63
-        "-'s (kate's camera / my brother's car etc.)", //64
-        "a / an ...", //65
-        "train(s) bus(es) (singular and plural)", //66
-        "a bottle / some water (countable / uncountable 1)", //67
-        "a cake / some cake / some cakes (countable / uncountable 2)", //68
-        "a / an and the", //69
-        "the ...", //70
-        "go to work go home go to the cinema", //71
-        "I like music I hat exam", //72
-        "the ... (names of places)", //73
-        "this / that / these / those", //74
-        "one / ones", //75
-        "some and any", //76
-        "not + any no none", //77
-        "not + anybody / anyone / anything nobody / no-one / nothing", //78
-        "somebody / anything / nowhere etc.", //79
-        "every and all", //80
-        "all most some any no/none", //81
-        "both either neither", //82
-        "a lot much many", //83
-        "(a) little (a) few",  //84
-        "old/nice/interesting etc. (adjectives)", //85
-        "quickly/badly/suddenly etc. (adverbs)", //86
-        "old/older expensive/more expensive", //87
-        "older than... more expensive than...", //88
-        "not as...as", //89
-        "the oldest the most expensive", //90
-        "enough", //91
-        "too", //92
-        "He speaks English very well. (word order 1)", //93
-        "always/usually/ofter etc. (word order 2)", //94
-        "still yet already", //95
-        "Give me that book! Give it to me!", //96
-        "and but or so because", //97
-        "When...", //98
-        "If we go... If you see... etc.", //99
-        "If I had... If we went... etc.", //100
-        "a person who... a thing that/which... (relative clauses 1)", //101
-        "the person we met the hotel you stayed at (relative clauses 2)", //102
-        "at 8 o'click on Monday in April", //103
-        "from ... to until since for", //104
-        "before after during while", //105
-        "in at on (places 1)", //106
-        "in at on (places 2)", //107
-        "to in at (places 3)", //108
-        "under, behind, opposite etc.", //109
-        "up, over, through etc.", //110
-        "on at by with about", //111
-        "afraid of..., good at... etc. of/at/for etc. (prepositions) + ing", //112
-        "listen to..., look at... etc. (verb + preposition)", //113
-        "go in, fall off, run away etc. (phrasal verbs 1)", //114
-        "put on your shoes put your shoes on (phrasal verbs 2)" //115 
+        {id:51, link:"", name:"work / working go / going do / doing"}, //51
+        {id:52, link:"", name:"to ... (I want to do) and -ing (I enjoy doing)"}, //52
+        {id:53, link:"", name:"I want to ... I told you to ..."}, //53
+        {id:54, link:"", name:"I went to the shop to ..."}, //54
+        {id:55, link:"", name:"go to ... go on .. go for ... go -ing"}, //55
+        {id:56, link:"", name:"get"}, //56
+        {id:57, link:"", name:"do and make"}, //57
+        {id:58, link:"", name:"have"}, //58
+        {id:59, link:"", name:"I / me he / him they / them etc."}, //59
+        {id:60, link:"", name:"my / his / their etc."}, //60
+        {id:61, link:"", name:"Whose is this? It's mine / yours / hers etc."}, //61
+        {id:62, link:"", name:"I / me / my / mine"}, //62
+        {id:63, link:"", name:"myself / yourself / themselves etc."}, //63
+        {id:64, link:"", name:"-'s (kate's camera / my brother's car etc.)"}, //64
+        {id:65, link:"", name:"a / an ..."}, //65
+        {id:66, link:"", name:"train(s) bus(es) (singular and plural)"}, //66
+        {id:67, link:"", name:"a bottle / some water (countable / uncountable 1)"}, //67
+        {id:68, link:"", name:"a cake / some cake / some cakes (countable / uncountable 2)"}, //68
+        {id:69, link:"", name:"a / an and the"}, //69
+        {id:70, link:"", name:"the ..."}, //70
+        {id:71, link:"", name:"go to work go home go to the cinema"}, //71
+        {id:72, link:"", name:"I like music I hat exam"}, //72
+        {id:73, link:"", name:"the ... (names of places)"}, //73
+        {id:74, link:"", name:"this / that / these / those"}, //74
+        {id:75, link:"", name:"one / ones"}, //75
+        {id:76, link:"", name:"some and any"}, //76
+        {id:77, link:"", name:"not + any no none"}, //77
+        {id:78, link:"", name:"not + anybody / anyone / anything nobody / no-one / nothing"}, //78
+        {id:79, link:"", name:"somebody / anything / nowhere etc."}, //79
+        {id:80, link:"", name:"every and all"}, //80
+        {id:81, link:"", name:"all most some any no/none"}, //81
+        {id:82, link:"", name:"both either neither"}, //82
+        {id:83, link:"", name:"a lot much many"}, //83
+        {id:84, link:"", name:"(a) little (a) few"},  //84
+        {id:85, link:"", name:"old/nice/interesting etc. (adjectives)"}, //85
+        {id:86, link:"", name:"quickly/badly/suddenly etc. (adverbs)"}, //86
+        {id:87, link:"", name:"old/older expensive/more expensive"}, //87
+        {id:88, link:"", name:"older than... more expensive than..."}, //88
+        {id:89, link:"", name:"not as...as"}, //89
+        {id:90, link:"", name:"the oldest the most expensive"}, //90
+        {id:91, link:"", name:"enough"}, //91
+        {id:92, link:"", name:"too"}, //92
+        {id:93, link:"", name:"He speaks English very well. (word order 1)"}, //93
+        {id:94, link:"", name:"always/usually/ofter etc. (word order 2)"}, //94
+        {id:95, link:"", name:"still yet already"}, //95
+        {id:96, link:"", name:"Give me that book! Give it to me!"}, //96
+        {id:97, link:"", name:"and but or so because"}, //97
+        {id:98, link:"", name:"When..."}, //98
+        {id:99, link:"", name:"If we go... If you see... etc."}, //99
+        {id:100, link:"", name:"If I had... If we went... etc."}, //100
+        {id:101, link:"", name:"a person who... a thing that/which... (relative clauses 1)"}, //101
+        {id:102, link:"", name:"the person we met the hotel you stayed at (relative clauses 2)"}, //102
+        {id:103, link:"", name:"at 8 o'click on Monday in April"}, //103
+        {id:104, link:"", name:"from ... to until since for"}, //104
+        {id:105, link:"", name:"before after during while"}, //105
+        {id:106, link:"", name:"in at on (places 1)"}, //106
+        {id:107, link:"", name:"in at on (places 2)"}, //107
+        {id:108, link:"", name:"to in at (places 3)"}, //108
+        {id:109, link:"", name:"under, behind, opposite etc."}, //109
+        {id:110, link:"", name:"up, over, through etc."}, //110
+        {id:111, link:"", name:"on at by with about"}, //111
+        {id:112, link:"", name:"afraid of..., good at... etc. of/at/for etc. (prepositions) + ing"}, //112
+        {id:113, link:"", name:"listen to..., look at... etc. (verb + preposition)"}, //113
+        {id:114, link:"", name:"go in, fall off, run away etc. (phrasal verbs 1)"}, //114
+        {id:115, link:"", name:"put on your shoes put your shoes on (phrasal verbs 2)"} //115 
     ];
 
 }
