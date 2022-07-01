025b75b9008eb6a7fc192c2fe337fe955157378f
sannko <sannkokooo@gmail.com>
2022-07-01 06:44:21 +0630

    Initial commit

--- src/app/grammar/data.ts
+++ src/app/grammar/data.ts
@@ -1,56 +1,56 @@
 export class Data {
 
     courseContent = [
-        "am / is / are", // 1
-        "am / is / are (questions)", // 2
-        "I am doing (present continous)", //3 
-        "are you doing? (present continous question)", //4 
-        "I do / work / like etc. (present simple)", //5
-        "I don't ... (present simple negative)", //4
-        "Do you ... ? (present simple questions)", //7
-        "I am doing (present continous) and I do (present simple)", //8
-        "I have ... and I've got ...", //9
-        "was / were", //10
-        "worked / got / went etc. (past simple)", //11
-        "I didn't ... Did you ...?(past simple negative and questions)", //12
-        "I was doing (past continous)", //13
-        "I was doing (past continous) and I did (past simple)", //14
-        "I have done (present perfect 1)", //15
-        "I've just ... I've already ... I haven't ... yet (present perfect 2)", //16
-        "Haver you ever ... ? (present perfect 3)", //17
-        "How long have you ... ? (present perfect 4)", //18
-        "for since ago", //19
-        "I have done(present perfect) and I did (past)", //20
-        "is done was done (passive 1)", //21
-        "is being done has been done (passive 2)", //22
-        "be / have / do in present and past tenses", //23
-        "Regular and irregular verbs", //24
-        "What are you doing tomorrow?", //25
-        "I'm going to ...", //26
-        "will / shall 1", //27
-        "will / shall 2", //28
-        "might", //29
-        "can and could", //30
-        "must mustn't don't need to", //31
-        "should", //32
-        "I have to ...", //33
-        "Would you like ... ? I'd like ...", //34
-        "Do this! Don't do that! Let's do this!", //35
-        "I used to ...", //36
-        "there is there are", //37
-        "there was / were there has / have been there will be", //38
-        "It ...", //39
-        "I am, i don't etc.", //40
-        "Have you? Are you? Don't you? etc.", //41
-        "too / either so am I / neither do I etc.", //42
-        "isn't, haven't, don't, etc. (negatives)", //43
-        "is it ... ? have you ... ? do they ... ? etc. (questions 1)", //44
-        "Who saw you? Who did you see? (questions 2)", //45
-        "Who is talking to? What is it like? (questions 3)", //46
-        "What ...? Which ... ? How ... ? (questions 4)", //47
-        "How long does it take ... ?", // 48
-        "Do you know where ... ? I don't know what ... etc", //49
-        "She said that ... He told me that ...", //50
+        {id:1, link:"", name:"am / is / are"}, // 1
+        {id:2, link:"", name:"am / is / are (questions)"}, // 2
+        {id:3, link:"", name:"I am doing (present continous)"}, //3 
+        {id:4, link:"", name:"are you doing? (present continous question)"}, //4 
+        {id:5, link:"", name:"I do / work / like etc. (present simple)"}, //5
+        {id:6, link:"", name:"I don't ... (present simple negative)"}, //4
+        {id:7, link:"", name:"Do you ... ? (present simple questions)"}, //7
+        {id:8, link:"", name:"I am doing (present continous) and I do (present simple)"}, //8
+        {id:9, link:"", name:"I have ... and I've got ..."}, //9
+        {id:10, link:"", name:"was / were"}, //10
+        {id:11, link:"", name:"worked / got / went etc. (past simple)"}, //11
+        {id:12, link:"", name:"I didn't ... Did you ...?(past simple negative and questions)"}, //12
+        {id:13, link:"", name:"I was doing (past continous)"}, //13
+        {id:14, link:"", name:"I was doing (past continous) and I did (past simple)"}, //14
+        {id:15, link:"", name:"I have done (present perfect 1)"}, //15
+        {id:16, link:"", name:"I've just ... I've already ... I haven't ... yet (present perfect 2)"}, //16
+        {id:17, link:"", name:"Haver you ever ... ? (present perfect 3)"}, //17
+        {id:18, link:"", name:"How long have you ... ? (present perfect 4)"}, //18
+        {id:19, link:"", name:"for since ago", //19
+        {id:20, link:"", name:"I have done(present perfect) and I did (past)"}, //20
+        {id:21, link:"", name:"is done was done (passive 1)"}, //21
+        {id:22, link:"", name:"is being done has been done (passive 2)"}, //22
+        {id:23, link:"", name:"be / have / do in present and past tenses"}, //23
+        {id:24, link:"", name:"Regular and irregular verbs"}, //24
+        {id:25, link:"", name:"What are you doing tomorrow?"}, //25
+        {id:26, link:"", name:"I'm going to ..."}, //26
+        {id:27, link:"", name:"will / shall 1"}, //27
+        {id:28, link:"", name:"will / shall 2"}, //28
+        {id:29, link:"", name:"might"}, //29
+        {id:30, link:"", name:"can and could"}, //30
+        {id:31, link:"", name:"must mustn't don't need to"}, //31
+        {id:32, link:"", name:"should"}, //32
+        {id:33, link:"", name:"I have to ..."}, //33
+        {id:34, link:"", name:"Would you like ... ? I'd like ..."}, //34
+        {id:35, link:"", name:"Do this! Don't do that! Let's do this!"}, //35
+        {id:36, link:"", name:"I used to ..."}, //36
+        {id:37, link:"", name:"there is there are"}, //37
+        {id:38, link:"", name:"there was / were there has / have been there will be"}, //38
+        {id:39, link:"", name:"It ..."}, //39
+        {id:40, link:"", name:"I am, i don't etc."}, //40
+        {id:41, link:"", name:"Have you? Are you? Don't you? etc."}, //41
+        {id:42, link:"", name:"too / either so am I / neither do I etc."}, //42
+        {id:43, link:"", name:"isn't, haven't, don't, etc. (negatives)"}, //43
+        {id:44, link:"", name:"is it ... ? have you ... ? do they ... ? etc. (questions 1)"}, //44
+        {id:45, link:"", name:"Who saw you? Who did you see? (questions 2)"}, //45
+        {id:46, link:"", name:"Who is talking to? What is it like? (questions 3)"}, //46
+        {id:47, link:"", name:"What ...? Which ... ? How ... ? (questions 4)"}, //47
+        {id:48, link:"", name:"How long does it take ... ?"}, // 48
+        {id:49, link:"", name:"Do you know where ... ? I don't know what ... etc"}, //49
+        {id:50, link:"", name:"She said that ... He told me that ..."}, //50
         "work / working go / going do / doing", //51
         "to ... (I want to do) and -ing (I enjoy doing)", //52
         "I want to ... I told you to ...", //53
