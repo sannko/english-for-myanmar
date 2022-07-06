fe2aa3136e3f35623d442d17aaf54d4ee8ca9a2b
sannko <sannkokooo@gmail.com>
2022-07-06 06:57:34 +0630

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBJwiGww1ju8-Pxv70sbqqscjYd_p7GTwA",
+    authDomain: "greattechpro.firebaseapp.com",
+    projectId: "greattechpro",
+    storageBucket: "greattechpro.appspot.com",
+    messagingSenderId: "574879234125",
+    appId: "1:574879234125:web:7e06a7dff77eca320bc404"
+  }
 };
